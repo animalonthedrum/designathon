3541c28d3b4405d7dbd2b1c4f1b981a83625e3a7
Brett Miller <brett.miller@sportngin.com>
2019-01-04 11:29:27 -0600

    pushing

--- src/components/_pages/Home.js
+++ src/components/_pages/Home.js
@@ -7,7 +7,11 @@ export default class Home extends Component {
   render() {
     return (
       <div className="homeContainer">
-        <h1>Home</h1>
+        <h1>Purrfect Match</h1>
+        <p>If you’re thinking about adopting a cat or kitten, Feline Rescue is a smart choice. By adopting a Feline Rescue pet you’ll be supporting our caring, no-kill organization and making room for one more homeless cat. We’re always looking for good, safe, loving homes for our many cats and kittens.
+
+All of our cats receive high-quality veterinary care and exams. Prior to adoption, all of our cats and kittens are FeLV/FIV tested; spayed/neutered, treated for external and internal parasites, microchipped, and FVRCP (distemper) vaccinated. We charge an adoption fee to offset part of these costs.</p>
+        <div>Cat Image here </div>
       </div>
     )
   }
